81c6690609397a57a7d236485d67f7ed669999cc
Andrew <verden.kma@gmail.com>
2021-03-16 19:06:44 +0200

    -initial commit

---
+++ lab-7-onwards/src/test/java/edu/ukma/javaee/lab7onwards/Lab7OnwardsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ukma.javaee.lab7onwards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab7OnwardsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
